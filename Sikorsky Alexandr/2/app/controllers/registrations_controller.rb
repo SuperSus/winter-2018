50cf4950243f7af8b0475f3cbf0a1f347acd738e
Aliaksandr Sikorski <PROFITERO\sikorski.a@pf-pc-85.profitero.local>
2019-03-04 10:39:11 +0300

    add seed data

--- Sikorsky Alexandr/2/app/controllers/registrations_controller.rb
+++ Sikorsky Alexandr/2/app/controllers/registrations_controller.rb
@@ -6,7 +6,6 @@ class RegistrationsController < Devise::RegistrationsController
     end
   
     def account_update_params
-        binding.pry
-        params.require(:user).permit(:first_name, :last_name, :email, :age, :weight, :height, :password, :password_confirmation, :current_password)
+      params.require(:user).permit(:first_name, :last_name, :email, :age, :weight, :height, :password, :password_confirmation, :current_password)
     end
 end
